c4a9b89574fd3b2ff585ffc02ba704bc37aec758
NickOttrando <nottrando@gmail.com>
2014-11-12 22:33:17 -0800

    added function to fix URL for requests, began getTrackID function

--- src/mmAPI.cpp
+++ src/mmAPI.cpp
@@ -1,5 +1,8 @@
 #include "mmAPI.h"
 #include <iostream>
+#include "ofApp.h"
+#include <string>
+#include <algorithm>
 using namespace std;
 
 const std::string mmAPI::API_KEY = "aae122dc24c48011f4afebf432ecfc8e";
@@ -13,8 +16,27 @@ mmAPI::~mmAPI(void)
 {
 }
 
+string mmAPI::fixURL(string oldURL){
+	int i = 0;
+	for (;;) {
+		i = oldURL.find(" ", i);
+		if (i == string::npos) {
+			break;
+		}
+		oldURL.replace(i, 1, "%20");
+	}
+	return oldURL;
+}
+
 int mmAPI::getTrackID(string trackName, string artistName){
 	//https://developer.musixmatch.com/documentation/api-reference/track-search
+	//ofHttpResponse resp = ofLoadURL("http://api.musixmatch.com/ws/1.1/track.search?q_track=back%20to%20december&q_artist=taylor%20swift&f_has_lyrics=1&apikey="+API_KEY);
+	trackName = fixURL(trackName);
+	artistName = fixURL(artistName);
+	string url = "http://api.musixmatch.com/ws/1.1/track.search?q_track="+trackName+"&q_artist="+artistName+"&f_has_lyrics=1&apikey="+API_KEY;
+	ofHttpResponse resp = ofLoadURL(url);
+	//cout << resp.data.getText() << endl;
+	//parse data to get track_id
 	return 1;
 }
 
